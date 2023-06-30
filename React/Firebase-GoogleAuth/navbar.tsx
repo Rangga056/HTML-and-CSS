eea86b8ba9731af5862c20794f01de044fca9fc8
Rangga Falah <136163122+Rangga056@users.noreply.github.com>
2023-06-30 18:09:48 +0700

    Create navbar.tsx
    Navbar component

---
+++ React/Firebase-GoogleAuth/navbar.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
+import "./style/navbar.css"
+
+export const Navbar = () => {
+  const [user] = useAuthState(auth);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+  }
+
+  return (
+    <div className="Navbar">
+      <Link to="/" > Home </Link>
+      <Link to="/login" > Login </Link>
+
+      <div>
+        {user && (
+          <>
+            <p>{user?.displayName} </p>
+            <img src={user?.photoURL || ""} width="30px" height="30px" />
+            <button onClick={signUserOut}>Sign Out</button>
+          </>
+          )
+        }
+      </div>
+    </div>
+  )
+}
